2047becb20410ec7302767c23fb5e9f2ace55891
Saajan Raj Pandey <saajan.pandey@ekbana.info>
2021-09-09 13:10:09 +0545

    API CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule} from '@nestjs/typeorm';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+        type:'mysql',
+        host:'localhost',
+        port: 3306,
+        username: 'root',
+        password:'nepal123',
+        database:'testnestjs',
+        autoLoadEntities: true,
+        synchronize:true,
+      }),
+      ContactsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
